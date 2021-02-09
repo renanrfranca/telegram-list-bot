f98b52e6abda7136f450da07911bd02266bdbc80
Renan Rossignatti de França <renanrfranca@gmail.com>
2021-02-09 15:48:01 -0300

    Innitial test commmit

---
+++ src/main/java/com/github/renanrfranca/telegramlistbot/ListBot.java
@@ -0,0 +1,52 @@
+package com.github.renanrfranca.telegramlistbot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class ListBot extends TelegramLongPollingBot {
+    private static final Logger logger = LoggerFactory.getLogger(ListBot.class);
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            SendMessage response = new SendMessage();
+            Long chatId = message.getChatId();
+            response.setChatId(String.valueOf(chatId));
+            String text = message.getText();
+            response.setText(text);
+            try {
+                execute(response);
+                logger.info("Sent message \"{}\" to {}", text, chatId);
+            } catch (TelegramApiException e) {
+                logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+            }
+        }
+    }
+
+
+}
