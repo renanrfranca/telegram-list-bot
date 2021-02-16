949ff8212cefc5320ee0056af5f5e48c9c99f3ed
Renan Rossignatti de França <renanrfranca@gmail.com>
2021-02-16 15:04:05 -0300

    basic AbilityBot configuration

--- src/main/java/com/github/renanrfranca/telegramlistbot/ListBot.java
+++ src/main/java/com/github/renanrfranca/telegramlistbot/ListBot.java
@@ -1,75 +1,37 @@
 package com.github.renanrfranca.telegramlistbot;
 
-import com.github.renanrfranca.telegramlistbot.commands.Command;
-import com.github.renanrfranca.telegramlistbot.commands.CommandFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.regex.Matcher;
+import org.telegram.abilitybots.api.bot.AbilityBot;
 import java.util.regex.Pattern;
 
 @Component
-public class ListBot extends TelegramLongPollingBot {
-    public static final String BOT_HANDLE = "@rrflistbot";
-
-    public static final Pattern COMMAND_PATTERN = Pattern.compile("/(\\w+)");
-    public static final int MAX_ROLL =  999999;
-
+public class ListBot extends AbilityBot {
     public static final Logger logger = LoggerFactory.getLogger(ListBot.class);
 
-    private final String token = System.getenv("TELEGRAM_BOT_TOKEN");
-    private final String username = System.getenv("TELEGRAM_BOT_USERNAME");
+    public static final String BOT_HANDLE = "@rrflistbot";
+    public static final int MAX_ROLL =  999999;
+    private static final String BOT_TONKEN = System.getenv("TELEGRAM_BOT_TOKEN");
+    private static final String BOT_USERNAME = System.getenv("TELEGRAM_BOT_USERNAME");
+
+    protected ListBot() {
+        super(BOT_TONKEN, BOT_USERNAME);
+    }
 
     @Override
     public String getBotToken() {
-        return token;
+        return BOT_TONKEN;
     }
 
     @Override
     public String getBotUsername() {
-        return username;
+        return BOT_USERNAME;
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            Message message = update.getMessage();
-            String text = message.getText();
-
-            if (hasCommand(text)) {
-                Command command;
-                try {
-                    command = CommandFactory.create(getCommandFromText(text));
-                    logger.info("Executando comando:" + text);
-                    command.handle(message);
-                } catch (
-                    IllegalAccessException |
-                    InstantiationException |
-                    NoSuchMethodException |
-                    InvocationTargetException e
-                ) {
-                    logger.error(e.getMessage());
-                }
-            }
-        }
+    public int creatorId() {
+        return 1392938098;
     }
 
-    private boolean hasCommand(String text) {
-        Matcher m = COMMAND_PATTERN.matcher(text);
-        return m.find();
-    }
-
-    private String getCommandFromText(String text) {
-        Matcher m = COMMAND_PATTERN.matcher(text);
-        if (m.find()) {
-            return m.group(1);
-        }
-
-        return null;
-    }
 }
