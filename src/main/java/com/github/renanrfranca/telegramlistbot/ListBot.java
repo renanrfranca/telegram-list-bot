6aa8d7649c0f362a98b1f5bac88fc064c1e03b01
Renan Rossignatti de França <renanrfranca@gmail.com>
2021-02-16 16:23:34 -0300

    help command

--- src/main/java/com/github/renanrfranca/telegramlistbot/ListBot.java
+++ src/main/java/com/github/renanrfranca/telegramlistbot/ListBot.java
@@ -1,10 +1,14 @@
 package com.github.renanrfranca.telegramlistbot;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
-import java.util.regex.Pattern;
+import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Locality;
+import org.telegram.abilitybots.api.objects.Privacy;
+import org.telegram.abilitybots.api.sender.MessageSender;
 
 @Component
 public class ListBot extends AbilityBot {
@@ -34,4 +38,19 @@ public class ListBot extends AbilityBot {
         return 1392938098;
     }
 
+    public Ability showHelp() {
+        return Ability.builder()
+                .name("help")
+                .info("Display help text")
+                .privacy(Privacy.PUBLIC)
+                .locality(Locality.ALL)
+                .input(0)
+                .action(ctx -> silent.send("Help text", ctx.chatId()))
+                .build();
+    }
+
+    @VisibleForTesting
+    void setSender(MessageSender sender) {
+        this.sender = sender;
+    }
 }
